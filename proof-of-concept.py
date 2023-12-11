b1ee7de9fc396fbbb0d23a37f01b59d26535bc35
Temesgen Ataro <atarotemesgen@gmail.com>
2023-12-11 18:13:41 +0400

    Final presentation prototype
    Contains code for displaying a spectrogram on 8x16 LED matrix as well as
    a GUI for playback on a monitor (buggy!).

---
+++ proof-of-concept.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+import sys
+import os.path
+
+
+audio_file = "never.wav"
+
+
+def read_audio(audio_file):
+    sr, arr = wavfile.read(audio_file)
+    if arr.shape[0] == 2:  # for 2 channel audio sources, only use one channel
+        arr = np.array(arr[1])
+    return sr, arr
+
+
+def make_fft_buckets(audio, buckets, sr):
+    """
+    Take an audio array and perform an FFT. Then spread the contents of
+    sr/len(audio) frequency bins into a pre-determined number of larger
+    buckets.
+    returns a histogram of FFT values with their energies.
+    """
+    # TODO: Demonstrate Parseval's Theorem by finding the energy in the
+    # time domain signal and the total energy stored in the frequency buckets.
+
+    A = np.fft.fftshift(np.fft.fft(audio))
+    N = len(A)
+    freq = np.arange(sr / -2, sr / 2, sr / len(audio))  # our FFT frequency bins
+
+    total_energy = np.sum(np.square(np.abs(A)))  # sum of energy in signal
+    # SUM[ ||x(k)||^2 ]
+    bucket_energies = [
+        np.sum(np.square(np.abs(A[i * N // buckets : (i + 1) * N // buckets])))
+        for i in range(buckets)
+    ]
+    # print("Here is the energy per bucket: ")
+    # print(*bucket_energies, sep="\n")
+    # print(
+    #     f"CHECK. Total energy: {total_energy:0.4f}, sum of energy in buckets: {np.sum(bucket_energies)}"
+    # )
+    new_bins = [f"{x * sr // buckets} Hz" for x in range(buckets)]
+
+    return new_bins, 20 * np.log10(bucket_energies)
+
+
+def segment_audio(arr, ms_segments, pos, sr=44_100, windowing="none"):
+    """
+    Takes an array and yields a specific slice of it to be processed.
+    # of samples = ms_segments * sr / 1000
+    """
+    seg_size = int(ms_segments * sr / 1000)
+    segments = int(arr.size // seg_size)
+    # print(
+    #     f"Total_audio_length {arr.size / sr} seconds. Segment_Length {seg_size / sr} seconds. #_of_segments {segments}"
+    # )
+    # TODO: Implement some sectin overlap or windowing in output
+    match windowing:
+        case "hamming":
+            return arr[pos * seg_size : (pos + 1) * seg_size] * np.hamming(seg_size)
+        case "hanning":
+            return arr[pos * seg_size : (pos + 1) * seg_size] * np.hanning(seg_size)
+        case "bartlett":
+            return arr[pos * seg_size : (pos + 1) * seg_size] * np.bartlett(seg_size)
+        case "none":
+            return arr[pos * seg_size : (pos + 1) * seg_size]
+
+
+def display_graphs(a, sr):
+    ms_segments = 500
+    seg_size = int(ms_segments * sr / 1000)
+    num_segments = int(a.size // seg_size)
+    for i in range(num_segments):
+        section = segment_audio(
+            a, ms_segments, i, sr, windowing="hamming"
+        )  # select a random segment to test
+        t = np.arange(len(section)) / sr
+        # Plot signal in time
+        plt.plot(t, section)
+        plt.xlabel("Time in seconds")
+        plt.ylabel("Signal Amplitude")
+        plt.title(f"Channel 1 of {audio_file}")
+        plt.show()
+
+        bins, A = make_fft_buckets(section, 10, sr)
+        # Plot FFT
+        plt.bar(bins, A)  # only plotting positive frequencies
+        plt.show()
+
+
+def main():
+    if not os.path.exists(audio_file):
+        print("File not found!")
+        return 0
+
+    sr, a = read_audio(audio_file)
+    print(f"sample rate: {sr}, array length: {len(a)}")
+    display_graphs(a, sr)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ src/hmain.py
@@ -0,0 +1,252 @@
+# Project libraries
+from processing import *
+
+# External libraries (installed and built-in)
+from scipy.io import wavfile
+import sounddevice as sd
+from PIL import Image, ImageDraw, ImageFont
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+import tkinter as tk
+import numpy as np
+from itertools import count
+import time
+import math
+import os
+
+TIME_INTERVAL = 0.1  # Represents the duration (in sec) of each chunk to be processed.
+# A TIME_INTERVAL of 0.1 s means a processing rate of 10 FPS.
+NUM_BUCKETS = 8  # Represents the number of frequency buckets in the LED matrix.
+HEIGHT_BUCKETS = 8  # Represents the height of each frequency bucket in the LED matrix.
+NUM_KNOBS = 8  # Represents the number of potentiometer knobs available.
+BUCKETS_PER_PTM = NUM_BUCKETS // NUM_KNOBS
+PATH = "never.wav"  # Path to the audio file. Must be a .wav file.
+
+
+def paint_led_matrix(
+    spectrum: np.array, device: max7219, width: int = 16, height: int = 8
+) -> None:
+    """
+    Paint the (one-sided) magnitude spectrum passed as a Numpy array onto a
+    width x height LED matrix. "width" represents the number of frequency
+    buckets and "height" represents the number of LEDs per bucket available for
+    display. The spectrum is expected to be provided for a real array of
+    magnitudes from 0 to 1.
+    """
+
+    # Downsize the spectrum to fit "width" buckets.
+    step = math.ceil(len(spectrum) / width)
+    spectrum = np.hstack((spectrum, np.zeros(step * width - len(spectrum))))[
+        step // 2 :: step
+    ]
+    # Scale the spectrum to the height of the LED matrix and round to nearest
+    # integer.
+    spectrum = (np.rint(height * spectrum)).astype(int)
+
+    # Paint the LED matrix
+    In = list((2 ** spectrum) - 1)
+    x1, x2, y = [], [], []
+    N = len(In)
+    for i in range(N):
+        if i < N / 2:
+            x1.append(In[i])
+        else:
+            x2.append(In[i])
+
+    for i in range(len(x1)):
+        y.append(x1[i])
+        y.append(x2[i])
+
+    image = y[::-1]
+    image_data = bytes(image)
+    img = Image.frombytes("1", (width, height), image_data)
+    device.display(img)
+
+    return spectrum
+
+
+def read_potentiometers(
+    knobs: list[tk.Scale],
+    knob_max: float,
+    width: int = 4,
+    height: int = 2,
+    buckets_per_ptm: int = 2,
+) -> np.array:
+    """
+    Read the values input by the potentiometers into a NumPy array representing
+    the amplification (EQ) to be applied to each frequency bucket. "width"
+    represents the number of potentiometers available, while "buckets_per_ptm"
+    represents how many frequency buckets are controlled by each potentiometer.
+    "height" represents the maximum amplification that can be applied to a
+    particular bucket. The return value is a width x buckets_per_ptm array
+    containing the respective amplifications per bucket.
+    """
+
+    # Read values from potentiometer knobs normalized from 0 to 1.
+    potentiometers = np.array([knob.get() for knob in knobs]) / float(knob_max)
+
+    # Scale the amplification array by the maximum amplification and resize to
+    # fit buckets.
+    potentiometers = height * potentiometers
+    potentiometers = np.repeat(potentiometers, buckets_per_ptm)
+
+    return potentiometers
+
+
+def main_pi(reverb: float = 0) -> None:
+    """
+    Main function for the entire input -> processing -> output pipeline for
+    equalizing audio from a .wav file. The audio is processed in intervals of
+    duration TIME_INTERVAL. "reverb" is an optional parameter to add a reverb
+    ratio from 0 to 1.
+    """
+
+    # Initialize LED matrix.
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=1, block_orientation=0, rotate=1)
+
+    # Initialize digital equalizer controls.
+    root = tk.Tk()
+    root.title("Graphic Equalizer (EQ) Controls")
+    knob_max = 200
+    title_label = tk.Label(root, text="Graphic Equalizer (EQ) Controls")
+    knobs = [
+        tk.Scale(root, from_=knob_max, to=0, width=20, length=200)
+        for i in range(NUM_KNOBS)
+    ]
+    labels = [tk.Label(root, text=f"Bin {i}") for i in range(NUM_KNOBS)]
+    title_label.grid(row=0, column=0, columnspan=8)
+    for i, (knob, label) in enumerate(zip(knobs, labels)):
+        knob.set(100)
+        knob.grid(row=1, column=i, padx=5, pady=5)
+        label.grid(row=2, column=i, padx=5, pady=5)
+
+    # Read the .wav audio file as a time series stored in "wave" with sampling
+    # rate "sr".
+    sr, wave = wavfile.read(PATH)
+
+    # Calculate the prototype band-pass filter response. This part only needs
+    # to be calculated once since the filter bank is just shifted copies of the
+    # filter. This makes processing much more efficient.
+    num_samples = int(TIME_INTERVAL * sr)
+    nfft = 2 ** math.ceil(math.log2(num_samples)) + 1
+    half_spectrum = (nfft + 1) // 2
+    width_filter = math.ceil(half_spectrum / NUM_BUCKETS)
+    prototype = band_pass_filter(
+        width_filter * NUM_BUCKETS, width_filter, width_filter // 2
+    )
+
+    # Play (non-blocking) the .wav file. The time series can be modified as it
+    # is played (not thread safe but is handy for our application).
+    sd.play(wave)
+    num_errors = 0
+
+    # Infinite loop with counter starting at 1
+    for i in count(1):
+        # Timer to deduct processing time
+        t0 = time.perf_counter()
+
+        # Update control window.
+        root.update()
+
+        # Window the current chunk. This program skips processing the first
+        # chunk.
+        start = i * num_samples
+        end = start + num_samples
+
+        # Add reverb by averaging with previous interval. Reverb must be
+        # between 0 and 1.
+        if reverb > 1 or reverb < 0:
+            raise Exception("Reverb ratio must be between 0 and 1.")
+        chunk_time = (1 - reverb) * wave[start:end] + reverb * wave[
+            start - num_samples : start
+        ]
+
+        # Apply the EQ to the waveform. Round up the number of samples to the
+        # nearest power of 2 then add 1 for the number of FFT points. This is
+        # because an even-numbered FFT does not have an equal number of
+        # components for positive and negative frequencies. We need to truncate
+        # the FFT to a one-sided FFT to apply the filter to the one-sided
+        # spectrum. Otherwise, applying the filter to the two-sided spectrum
+        # without errors is somewhat difficult to set up.
+        eq_input = read_potentiometers(
+            knobs, knob_max, width=NUM_BUCKETS, buckets_per_ptm=BUCKETS_PER_PTM
+        )
+        chunk_freq, _ = compute_spectrum(chunk_time, nfft=nfft)
+        new_chunk_freq = apply_eq(chunk_freq, eq_input, prototype)
+        new_chunk_time = compute_waveform(new_chunk_freq, num_samples=num_samples)
+        wave[start:end] = new_chunk_time
+
+        # Magnitude spectrum of filtered signal
+        new_chunk_freq = np.abs(new_chunk_freq)
+
+        # Used for scaling the spectrum for the LED matrix. As a rule of thumb,
+        # the spectrum is normalized with respect to mean + standard deviation.
+        # The spectrum is definitely not normally distributed, but it is used
+        # as a quick fix. Any value above that is clipped.
+        avg = np.mean(new_chunk_freq)
+        st_dev = np.std(new_chunk_freq)
+        norm = avg + st_dev
+        new_chunk_freq = np.clip((new_chunk_freq / norm), 0, 1)
+
+        # Paint the LED matrix.
+        paint_led_matrix(new_chunk_freq, device, NUM_BUCKETS, HEIGHT_BUCKETS)
+
+        # Timer to deduct processing time
+        t1 = time.perf_counter()
+        offset = t1 - t0
+
+        # Wait for TIME_INTERVAL seconds to play the current audio chunk
+        # (accounting for processing time). A variable is used to keep track of
+        # how many times the processing goes out of sync (error checking).
+        try:
+            time.sleep(TIME_INTERVAL - offset)
+        except ValueError as e:
+            num_errors += 1
+            print(num_errors)
+            if num_errors > 3:
+                raise e
+
+
+def main_pc():
+    root = tk.Tk()
+    root.title("Graphic Equalizer (EQ) Controls")
+    root.geometry("800x640")
+    title_label = tk.Label(root, text="Graphic Equalizer (EQ) Controls")
+
+    def show_play():
+        label.config(text=f"{clicked.get()} selected for playback")
+        file = clicked.get()
+        print(f"{file} selected")
+        root.destroy()
+        time.sleep(0.001)
+        play_on_pc(file)
+
+    def show_alter():
+        file = clicked.get()
+        print(f"{file} selected")
+        alter_on_pc(file)
+
+    files = [i for i in os.listdir() if i[-3:] == "wav"]
+
+    clicked = tk.StringVar(root)
+    clicked.set(files[0])
+    drop = tk.OptionMenu(root, clicked, *files)
+    drop.pack()
+    click_for_playback = tk.Button(
+        root, text="Select file to view periodogram", command=show_play
+    ).pack()
+    label = tk.Label(root, text=" ")
+    label.pack()
+    click_for_noising = tk.Button(
+        root, text="Select file to alter", command=show_alter
+    ).pack()
+    label = tk.Label(root, text=" ")
+    label.pack()
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main_pi()
